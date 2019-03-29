e1e645847ef2a6674e090363bcc7046ed7c0c103
SimiCode <abahedison1@outlook.com>
2019-03-29 12:37:30 +0300

    add server start

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const express = require('express');
 // const port = 3000;
 var port = process.env.PORT || 8080;
 
-
-const app = express();
 var bodyParser = require('body-parser');
-
-
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
